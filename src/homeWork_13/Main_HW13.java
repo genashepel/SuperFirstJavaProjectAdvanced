22fb3ce6bce5138528201e68602a113db338c5e0
Gena Shepel <genashepel@gmail.com>
2022-05-27 01:00:04 -0400

    Add reviue from groupe

--- src/homeWork_13/Main_HW13.java
+++ src/homeWork_13/Main_HW13.java
@@ -5,38 +5,86 @@ import java.util.*;
 public class Main_HW13 {
     public static void main(String[] args) {
 
-        List<String> collors = new ArrayList();
-        collors.add("White");
-        collors.add("Tan");
-        collors.add("Yellow");
-        collors.add("Orange");
-        collors.add("Red");
-        collors.add("Pink");
-        collors.add("Purple");
-        collors.add("Blue");
+        List<String> collors = new ArrayList<>();
+        collors.add("White.");
+        collors.add("Tan.");
+        collors.add("Yellow.");
+        collors.add("Black");
+        collors.add("Orange.");
+        collors.add("Red.");
+        collors.add("Pink.");
+        collors.add("Black");
+        collors.add("Purple.");
+        collors.add("Blue.");
 
+
+       //ArrayList<String> collors2 = new ArrayList<>(Arrays.asList("White", "Tan"));
         System.out.println(collors);
+        System.out.println(collors.size());
 
-        for (int i = 0; i < collors.size(); i++) {
-            if (collors.get(i).contains("l") || collors.get(i).contains("L")) {
+        //Version1
+        for (int i = collors.size() - 1; i >= 0; i--) {
+            if (collors.get(i).toLowerCase().contains("l")) {
                 collors.remove(i);
+                //i--;
             }
         }
+
+        //Version2
+        for (int i = 0; i < collors.size(); i++) {
+            if (collors.get(i).toLowerCase().contains("l")) {
+                collors.remove(i);
+                i--;
+            }
+        }
+        //Version3
+        //collors.removeIf(i -> i.toLowerCase().contains("l"));
+
         System.out.println(collors);
+        System.out.println("\n");
 
         //Task_2
         List <Integer> arrayOfInteger = new ArrayList<>();
         for(int i = 100; i <= 1000; i++) {
             arrayOfInteger.add(i);
         }
+        //System.out.println("List from 100 to 1000: " + arrayOfInteger);
 
-        System.out.println("List from 100 to 1000: " + arrayOfInteger);
+//        for(int i = 0; i < arrayOfInteger.size(); i++){
+//            if(arrayOfInteger.get(i) % 2 == 0){
+//                arrayOfInteger.remove(i);
+//                i--;
+//            }
+//        }
+//        System.out.println("List only odd " + arrayOfInteger);
+
+//        //Task_3
+//        List <Integer> arrayOfInteger_Task3 = new ArrayList<>();
+//        for(int i = 100; i <= 1000; i++) {
+//            arrayOfInteger_Task3.add(i);
+//        }
+//        System.out.println("List from 100 to 1000: " + arrayOfInteger_Task3);
+//        System.out.println(arrayOfInteger_Task3.size());
+//
+//        for(int i = 0; i < arrayOfInteger_Task3.size(); i++){
+//            if(arrayOfInteger_Task3.get(i) % 2 != 0){
+//                arrayOfInteger_Task3.remove(i);
+//                //i--;
+//            }
+//        }
+//        System.out.println("List only odd " + arrayOfInteger_Task3);
+
+
+        List<Integer> toRemove = new ArrayList();
 
         for(Integer i = 0; i < arrayOfInteger.size(); i++){
-            if(arrayOfInteger.get((int)i) % 2 == 0){
-                arrayOfInteger.remove((int)i);
+            if(arrayOfInteger.get(i) % 2 == 0) {
+                toRemove.add(arrayOfInteger.get(i));
             }
         }
-        System.out.println("List only odd " + arrayOfInteger);
+        arrayOfInteger.removeAll(toRemove);
+        System.out.println(arrayOfInteger);
+
     }
+
 }
