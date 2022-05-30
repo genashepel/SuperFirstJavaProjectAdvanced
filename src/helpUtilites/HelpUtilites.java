fc2ae6c49d30bf52782932509ffb2152d8a692c3
Gena Shepel <genashepel@gmail.com>
2022-05-30 08:32:12 -0400

    Add metod findNameFromSubString in HelpUtilites

--- src/helpUtilites/HelpUtilites.java
+++ src/helpUtilites/HelpUtilites.java
@@ -1,14 +1,18 @@
 package helpUtilites;
 
 import homework_7.Employee7_1;
+import homework_7.Manager7_1;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelpUtilites {
 
     public static  void printLine(){
-        System.out.println("____________________________");
+        System.out.println("____________________________________________");
     }
 
-     public static Employee7_1 findNameArrays(Employee7_1[] arr, String name){
+     public static Employee7_1 findNameFromName(Employee7_1[] arr, String name){
         Employee7_1 returnEmployee = null;
         for(int i = 0; i < arr.length; i++){
             if(arr[i].getName().equals(name)){
@@ -16,5 +20,27 @@ public class HelpUtilites {
             }
         }
         return returnEmployee;
+    }
+
+    public static  List <Employee7_1> findNameFromSubString(Employee7_1[] arr, String subString){
+        //Employee7_1 returnEmployee = null;
+        List<Employee7_1> listOfEmployee = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].getName().contains(subString)){
+                listOfEmployee.add(arr[i]);
+            }
+        }
+        return listOfEmployee;
+    }
+
+    public static  List <Manager7_1> findNameFromSubString(Manager7_1[] arr, String subString){
+
+        List<Manager7_1> listOfManager = new ArrayList<>();
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].getName().contains(subString)){
+                listOfManager.add(arr[i]);
+            }
+        }
+        return listOfManager;
     }
 }
--- src/homework_7/HW_7_1.java
+++ src/homework_7/HW_7_1.java
@@ -1,9 +1,12 @@
 package homework_7;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-import static helpUtilites.HelpUtilites.findNameArrays;
+
+import static helpUtilites.HelpUtilites.*;
 
 public class HW_7_1 {
     public static void main(String[] args){
@@ -12,13 +15,36 @@ public class HW_7_1 {
         Employee7_1 employee2 = new Employee7_1("Maryna", 500);
         Employee7_1 employee3 = new Employee7_1("Anna", 700);
 
+        Manager7_1 manager1 = new Manager7_1("Gennadiy", 1000, 5);
+        Manager7_1 manager2 = new Manager7_1("Maryna", 500, 10);
+        Manager7_1 manager3 = new Manager7_1("Anna", 700, 18);
+
+        Manager7_1[] arrOfManager = {manager1, manager2, manager3};
+        List<Manager7_1> listOfManagers = findNameFromSubString(arrOfManager, "Ge");
+
         Employee7_1[] arrOfEmployee = {employee1, employee2, employee3};
+        List<Employee7_1> listOfEmployee = findNameFromSubString(arrOfEmployee, "na");
 
-        Employee7_1 nameFromArrayOfEmployee = findNameArrays(arrOfEmployee, "Anna");
 
-        System.out.println(nameFromArrayOfEmployee.getName()
-                + " has salary "
-                + nameFromArrayOfEmployee.getSalary());
+        for(Employee7_1 list : listOfEmployee){
+            System.out.println("Employee " + list.getName() + " has salary ---> " + list.getSalary());
+        }
+
+        printLine();
+
+        for(Manager7_1 list : listOfManagers){
+            System.out.println("Manager " + list.getName() + " has salary ---> " + list.getSalary());
+        }
+//        System.out.println(listOfEmployee.get(0).getName());
+//        System.out.println(listOfEmployee.get(1).getName());
+
+        //Employee7_1 nameFromArrayOfEmployee = findNameArrays(arrOfEmployee, "Anna");
+        //Employee7_1 nameFromArrayOfEmployee1 = findNameFromSubString(arrOfEmployee, "enn");
+
+//        System.out.println(nameFromArrayOfEmployee1.getName()
+//                + " has salary "
+//                + nameFromArrayOfEmployee1.getSalary());
+
 
 //        Manager7_1 manager1 = new Manager7_1("Gena", 1000, 25);
 //
